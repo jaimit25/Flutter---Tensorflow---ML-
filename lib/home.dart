0acc3e16337a6acca3456a6ea9dc53b2cb6a68aa
Jaimit <jaimit.jp@gmail.com>
2022-04-15 23:09:32 +0530

    Machine learning - object detection

---
+++ lib/home.dart
@@ -0,0 +1,107 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:tensorflow/main.dart';
+import 'package:tflite/tflite.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  CameraImage? cameraImage;
+  CameraController? cameraController;
+  String output = '';
+
+  loadcamera() {
+    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
+    cameraController!.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {
+        cameraController!.startImageStream((image) => setState(() {
+              cameraImage = image;
+              runModel();
+            }));
+      });
+    });
+  }
+
+  runModel() async {
+    if (cameraImage != null) {
+      var predictions = await Tflite.runModelOnFrame(
+        bytesList: cameraImage!.planes.map((plane) {
+          return plane.bytes;
+        }).toList(),
+        imageHeight: cameraImage!.height,
+        imageWidth: cameraImage!.width,
+        numResults: 2,
+        rotation: 90,
+        threshold: 0.5,
+        imageMean: 127.5,
+        imageStd: 127.5,
+        asynch: true,
+      );
+
+      predictions!.forEach((element) {
+        setState(() {
+          output = element['label'];
+          print(output.toString());
+        });
+      });
+    }
+  }
+
+  loadmodel() async {
+    await Tflite.loadModel(
+      model: "assets/model.tflite",
+      labels: "assets/labels.txt",
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadcamera();
+    loadmodel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            // child: AspectRatio(
+            //   aspectRatio: cameraController!.value.aspectRatio,
+            //   child: CameraPreview(cameraController!),
+            // ),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width,
+              child: !cameraController!.value.isInitialized
+                  ? Container()
+                  : AspectRatio(
+                      aspectRatio: cameraController!.value.aspectRatio,
+                      child: CameraPreview(cameraController!),
+                    ),
+            ),
+          ),
+          Container(
+            child: Text(
+              output.toString(),
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:tensorflow/home.dart';
+
+List<CameraDescription>? cameras;
+
+void main() async{
+  await WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home:Scaffold(),
+      home: Home(),
+    );
+  }
+}
